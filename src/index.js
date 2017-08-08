1919848b4cd80e3b8889cc6f0c6a421a79da5ab3
shatalovdm <innoshatalov@gmail.com>
2017-08-08 12:02:34 -0500

    Configured webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import reducers from './reducers';
+// import promise from 'redux-promise';
+// import Property from './components/Property';
+// import NeighborhoodMap from './components/NeighborhoodMap';
+// import '../css/map.css';
+// import '../css/slick.css';
+import '../style/style.css';
+
+
+console.log('hey there!!');
+
+ReactDOM.render(
+  <div>Hello!</div>
+  , document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-router-dom',
+  'redux-thunk', 'redux-promise'
+];
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+};
